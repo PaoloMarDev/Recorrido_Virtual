f6d2764b6cc8d467d19560d787258e63947928e7
PaoloMar <PaoloMar_@hotmail.com>
2026-03-18 08:48:47 -0600

    Se creó y se separó los hotspots (y funciones que utilizan) del código index para tener mejor organización en el código.

--- recorrido-virtual-cicata/app-files/index.js
+++ recorrido-virtual-cicata/app-files/index.js
@@ -15,6 +15,9 @@
  */
 'use strict';
 
+import { stopTouchAndScrollEventPropagation } from "./hotspots/funcionesGenerales.js";
+import { createInfoHotspotElement } from "./hotspots/hsp_info.js";
+
 (function() {
   var Marzipano = window.Marzipano;
   var bowser = window.bowser;
@@ -244,7 +247,7 @@
     }
   }
 
-  function createLinkHotspotElement(hotspot) {
+  function createLinkHotspotElement(hotspot) { // Este no se puede poner en la carpeta de hotpots.
 
     // Create wrapper element to hold icon and tooltip.
     var wrapper = document.createElement('div');
@@ -283,110 +286,27 @@
 
     return wrapper;
   }
-
-  function createInfoHotspotElement(hotspot) {
-
-    // Create wrapper element to hold icon and tooltip.
-    var wrapper = document.createElement('div');
-    wrapper.classList.add('hotspot');
-    wrapper.classList.add('info-hotspot');
-
-    // Create hotspot/tooltip header.
-    var header = document.createElement('div');
-    header.classList.add('info-hotspot-header');
-
-    // Create image element.
-    var iconWrapper = document.createElement('div');
-    iconWrapper.classList.add('info-hotspot-icon-wrapper');
-    var icon = document.createElement('img');
-    icon.src = 'img/info.png';
-    icon.classList.add('info-hotspot-icon');
-    iconWrapper.appendChild(icon);
-
-    // Create title element.
-    var titleWrapper = document.createElement('div');
-    titleWrapper.classList.add('info-hotspot-title-wrapper');
-    var title = document.createElement('div');
-    title.classList.add('info-hotspot-title');
-    title.innerHTML = hotspot.title;
-    titleWrapper.appendChild(title);
-
-    // Create close element.
-    var closeWrapper = document.createElement('div');
-    closeWrapper.classList.add('info-hotspot-close-wrapper');
-    var closeIcon = document.createElement('img');
-    closeIcon.src = 'img/close.png';
-    closeIcon.classList.add('info-hotspot-close-icon');
-    closeWrapper.appendChild(closeIcon);
-
-    // Construct header element.
-    header.appendChild(iconWrapper);
-    header.appendChild(titleWrapper);
-    header.appendChild(closeWrapper);
-
-    // Create text element.
-    var text = document.createElement('div');
-    text.classList.add('info-hotspot-text');
-    text.innerHTML = hotspot.text;
-
-    // Place header and text into wrapper element.
-    wrapper.appendChild(header);
-    wrapper.appendChild(text);
-
-    // Create a modal for the hotspot content to appear on mobile mode.
-    var modal = document.createElement('div');
-    modal.innerHTML = wrapper.innerHTML;
-    modal.classList.add('info-hotspot-modal');
-    document.body.appendChild(modal);
-
-    var toggle = function() {
-      wrapper.classList.toggle('visible');
-      modal.classList.toggle('visible');
-    };
-
-    // Show content when hotspot is clicked.
-    wrapper.querySelector('.info-hotspot-header').addEventListener('click', toggle);
-
-    // Hide content when close icon is clicked.
-    modal.querySelector('.info-hotspot-close-wrapper').addEventListener('click', toggle);
-
-    // Prevent touch and scroll events from reaching the parent element.
-    // This prevents the view control logic from interfering with the hotspot.
-    stopTouchAndScrollEventPropagation(wrapper);
-
-    return wrapper;
-  }
-
-  // Prevent touch and scroll events from reaching the parent element.
-  function stopTouchAndScrollEventPropagation(element, eventList) {
-    var eventList = [ 'touchstart', 'touchmove', 'touchend', 'touchcancel',
-                      'wheel', 'mousewheel' ];
-    for (var i = 0; i < eventList.length; i++) {
-      element.addEventListener(eventList[i], function(event) {
-        event.stopPropagation();
-      });
-    }
-  }
-
-  function findSceneById(id) {
-    for (var i = 0; i < scenes.length; i++) {
-      if (scenes[i].data.id === id) {
-        return scenes[i];
+  
+    function findSceneById(id) {
+      for (var i = 0; i < scenes.length; i++) {
+        if (scenes[i].data.id === id) {
+          return scenes[i];
+        }
       }
+      return null;
     }
-    return null;
-  }
-
-  function findSceneDataById(id) {
-    for (var i = 0; i < data.scenes.length; i++) {
-      if (data.scenes[i].id === id) {
-        return data.scenes[i];
+  
+    function findSceneDataById(id) {
+      for (var i = 0; i < data.scenes.length; i++) {
+        if (data.scenes[i].id === id) {
+          return data.scenes[i];
+        }
       }
+      return null;
     }
-    return null;
-  }
 
   // Display the initial scene.
   switchScene(scenes[0]);
-
+  
 })();
+
