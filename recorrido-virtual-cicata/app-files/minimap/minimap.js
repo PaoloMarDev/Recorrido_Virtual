dbe4c2956c5848b7e2815e49c6e79d7feb2e6a52
Andres Peña <A01029309@tec.mx>
2026-03-24 11:49:00 -0600

    Agregar minimapa con layout responsivo e imagen de prueba.

---
+++ recorrido-virtual-cicata/app-files/minimap/minimap.js
@@ -0,0 +1,5 @@
+const minimapContainer = document.getElementById('minimap-container');
+
+minimapContainer.addEventListener('click', function() {
+    minimapContainer.classList.toggle('is-expanded');
+});
